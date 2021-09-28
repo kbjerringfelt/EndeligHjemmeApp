d3ae645109db50e67be7409af982efbb6f3a83eb
Karen <karen.bjerringfelt@gmail.com>
2021-09-28 13:15:41 +0200

    added button

--- app/src/main/java/dk/au/au339038/bachelorprojekt/endelighjemmeapp/MainActivity.java
+++ app/src/main/java/dk/au/au339038/bachelorprojekt/endelighjemmeapp/MainActivity.java
@@ -3,12 +3,16 @@ package dk.au.au339038.bachelorprojekt.endelighjemmeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
