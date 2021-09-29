99096a289d63e5dad1da6d39745aa44dd232dead
Karen <karen.bjerringfelt@gmail.com>
2021-09-29 09:51:48 +0200

    Added activities

---
+++ app/src/main/java/dk/au/au339038/bachelorprojekt/endelighjemmeapp/GroupListActivity.java
@@ -0,0 +1,14 @@
+package dk.au.au339038.bachelorprojekt.endelighjemmeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_list);
+    }
+}
