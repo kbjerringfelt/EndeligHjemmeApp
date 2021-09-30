5273c8c1e7e02a1366c2114ff26a75c85dafeaa2
Karen <karen.bjerringfelt@gmail.com>
2021-09-30 11:59:42 +0200

    Added more classes and some code to group

---
+++ app/src/main/java/dk/au/au339038/bachelorprojekt/endelighjemmeapp/Group.java
@@ -0,0 +1,14 @@
+package dk.au.au339038.bachelorprojekt.endelighjemmeapp;
+
+public class Group {
+    private String groupName;
+    private String groupDescription;
+    private String groupEMail;
+
+    public Group(String name, String email, String description){
+        this.groupName = name;
+        this.groupDescription = description;
+        this.groupEMail = email;
+    }
+
+}
---
+++ app/src/main/java/dk/au/au339038/bachelorprojekt/endelighjemmeapp/LogInViewModel.java
@@ -0,0 +1,4 @@
+package dk.au.au339038.bachelorprojekt.endelighjemmeapp;
+
+public class LogInViewModel {
+}
